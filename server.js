ecbd8a73ed4c02df45683ffad6b10802f2f1600d
aecuto <kampee.expertly01@gmail.com>
2020-09-17 14:23:21 +0700

    add git ignore

---
+++ server.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 8000,
+    host: "0.0.0.0",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      const query = request.query;
+      return {
+        msg: "Hello World!",
+        query,
+      };
+    },
+  });
+
+  server.route({
+    path: "/upload",
+    method: "POST",
+    options: {
+      payload: {
+        maxBytes: 1500000,
+        parse: true,
+        output: "stream",
+        multipart: true,
+      },
+    },
+    handler: async (req, h) => {
+      const { payload } = req;
+
+      return {
+        data: payload.file.hapi.filename,
+      };
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
